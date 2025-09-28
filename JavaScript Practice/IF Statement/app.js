db63a2c0914f282755fb2b9be576c2a9c3bd209a
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-09-28 02:02:03 -0900

    Statement are Done

--- JavaScript Practice/IF Statement/app.js
+++ JavaScript Practice/IF Statement/app.js
@@ -1,4 +1,4 @@
-console.log('JavaScript Connected!')
+// console.log('JavaScript Connected!');
 
 //   Old Way !
 
@@ -12,8 +12,118 @@ console.log('JavaScript Connected!')
 // if(userAge >= 3  && userAge <=8){
 //     console.log(`${userName}: is a kid`);
 // }
-    
 
 
+// var userName = prompt('Type your Name!')
+// var userAge = prompt('Type your Age!')
 
+// console.log(userName)
+// console.log(userAge)
+
+// debugger 
+
+// if ( userAge >= 3 && userAge <= 8) {
+//     console.log(`${userName}: is a kid`)
+//     console.log(`${userName}: is playing football`)
+// }
+// else if ( userAge >= 9 && userAge <= 18) {
+//     console.log(`${userName}: is a School Student`)
+//     console.log(`${userName}: is Learning Maths / English`)
+// }
+// else if ( userAge >= 19 && userAge <= 24) {
+//     console.log(`${userName}: is a College Student`)
+//     console.log(`${userName}: is Learning Computer Science`)
+// }
+// else if ( userAge >= 25 && userAge <= 50) {
+//     console.log(`${userName}: is a Working Professional`)
+//     console.log(`${userName}: is a Full Stack Devloper`)
+// }
+// else if (userAge >= 51) {
+//     console.log(`${userName}: is Retired!`)
+// }
+// else {
+//     console.log('Please enter a valid age!')
+// }       
+
+// console.log('Program Ended!')
+
+// var num =  +prompt('Type your Number!')
+
+// if (num % 2 === 0) {
+//     console.log('Its Number is Even!')
+// }
+// else {
+//     console.log('Its Number is odd!')
+// }
+
+// Whether Checker!
+
+// var check = prompt("Type your Number!")
+
+// if (check >= 80){
+//     console.log("Grade A")
+// }
+// else if (check >= 60){
+//     console.log("Grade B")
+// }
+// else if (check >= 40){
+//     console.log("Grade C")
+// }
+// else {
+//     console.log("Failed!")
+// }
+
+
+//  Traffic System!
+
+// var trafic = prompt("Choice a color Red / Green / Yellow");
+
+// if (trafic == 'Red'){
+//     console.log("Stop!")
+// }
+// else if (trafic == 'Yellow'){
+//     console.log("Wait!")
+// }
+// else if (trafic == 'Green'){
+//     console.log("Go!")
+// }
+// else {
+//     console.log("Choice a valid color!")
+// }
+
+// var num1 = +prompt("Enter Scince Subject Number!")
+// var num2 = +prompt("Enter Maths Subject Number!")
+// var num3 = +prompt("Enter Biology Subject Number!")
+
+// if (num1 >= 40 && num2 >= 40 && num3 >= 40) {
+//     console.log("You are Passed!")
+// }
+// else {
+//     console.log("Failed!")
+// }
+
+// var userName = prompt('Type your Name!')
+// var userAge = +prompt('Type your Age!')
+
+// if (userAge >= 3 && userAge <= 8 ){
+//     console.log(`${userName}: is Playing Cricket!`)
+// }
+// else if (userAge >= 9 && userAge <= 18 ){
+//     console.log(`${userName}: is School Student!`)
+// }
+// else if (userAge >= 19 && userAge <= 24 ){
+//     console.log(`${userName}: is College Student!`)
+//     if (userName.length > 5){
+//         console.log("Length of user name is Greater than 5")
+//     }
+//     else {
+//         console.log("Length of user name is less than 5")
+//     } 
+// }
+// else if (userAge >= 25 && userAge <= 50 ){
+//     console.log(`${userName}: is Working Professional!`)
+// } 
+// else {
+//     console.log(`${userName}: is no more`)
+// }
 
