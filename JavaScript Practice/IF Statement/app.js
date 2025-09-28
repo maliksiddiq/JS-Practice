5a2cd2082cae1078d73f0dfa1e10f6aabd418844
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-09-27 19:55:24 -0900

    Statement are continue

---
+++ JavaScript Practice/IF Statement/app.js
@@ -0,0 +1,19 @@
+console.log('JavaScript Connected!')
+
+//   Old Way !
+
+// console.log(userName + ' is a good man ')
+// console.log('Age of ' + ' ' +  userName  + ' ' + 'is ' + userAge )
+
+
+// var userName = prompt(' Type your Name: ')
+// var userAge = +prompt(' Type your Age: ')
+
+// if(userAge >= 3  && userAge <=8){
+//     console.log(`${userName}: is a kid`);
+// }
+    
+
+
+
+
---
+++ JavaScript Practice/Logical Operator/app.js
@@ -0,0 +1,144 @@
+console.log("JavaScript Connected!")
+
+// let num = 12 > 4;
+// console.log(num) 
+
+// let num1 = 12 < 19;
+// console.log(num1)
+
+// let num2 = 12;
+// let num3 = 12;
+
+// console.log(num2 <= num3)
+
+// let num4 = 12;
+// let num5 = 12;
+// console.log(num4 >= num5)
+
+// let num6 = 23;
+// let num7 = 23;
+
+// console.log(num6 == num7)
+
+// let num8 = 23;
+// let num9 = "23";
+
+// console.log(num8 === num9)
+
+// let num10 = 12;
+// let num11 = 13;
+
+// console.log(num10 != num11)
+
+// let num12 = 12;
+// let num13 = '12';
+
+// console.log(num12 !== num13)
+
+// let a = 10;
+// let b = '10';
+
+// console.log(a == b)
+// console.log(a === b)
+// console.log(a != b)
+// console.log(a !== b)
+// console.log(a > 8)
+// console.log(a < 8)
+// console.log(a <= 10)
+// console.log(a >= 12)
+
+
+//  Logical Operator 
+
+// 1: And Operator &&
+// 2: OR Operator  | |
+// 3:Not Opearator !
+
+// var userAge1 = 2;
+// var userAge2 = 18;
+
+// if(userAge1 > userAge2){
+//     console.log("Congrats")
+// }
+// else {
+//     console.log("Next Time")
+// }
+
+
+// var cond1 = 16 > 8; 
+// var cond2 = 16 < 8;
+// console.log(cond1)
+// console.log(cond2)
+
+// var cond1 = 16 > 8;
+// var cond2 = 5 < 8;
+
+
+// var resultAnd = cond1 && cond2;
+// console.log(resultAnd)
+
+// var cond1 = 16 > 7;
+// var cond2 = 16 < 12;
+
+// var resultAnd = cond1 || cond2 
+// console.log(resultAnd)
+
+
+// var cond1 = 16 > 7;
+// var cond2 = 16 < 12;
+
+// var resultAnd = cond1 && cond2 
+// console.log(resultAnd)
+
+// var cond1 = 18 < 22;
+// var cond2 = 22 > 18;
+// console.log(cond1)
+// console.log(cond2)
+
+// var resultAnd = cond1 && cond2;
+// console.log(resultAnd)
+
+// var cond1 = 12 < 5;
+// var cond2 = 12 > 4;
+// console.log(cond1)
+// console.log(cond2)
+
+// var resultAnd = cond1 && cond2
+// console.log(resultAnd)
+
+// var cond1 = 12 < 22;
+// var cond2 = 12 > 22;
+// console.log(cond1)
+// console.log(cond2)
+
+// var resultOr = cond1 || cond2
+// var resultAnd = cond1 && cond2
+// console.log(resultOr)
+// console.log(resultAnd)
+
+// var cond1 = 12 < 19;
+// console.log(!cond1)
+
+// var cond2 = 12 < 4;
+// console.log(!cond2)
+
+// var resultAnd = 12 > 5 && 12 < 13 && 14 < 12;
+// console.log(resultAnd) 
+
+// var resultOr =  12 > 13 || 14 < 12 || 12 > 2;
+// console.log(resultOr)
+
+// var cond1 = 0 && 'Hello';
+// console.log(cond1)
+
+// var cond1 = 0 || 'Hello';
+// console.log(cond1)
+
+// var cond1 = '' && "Hello"
+// console.log(cond1)
+
+// var cond2 = '' || "Hello"
+// console.log(cond2)
+
+
+
