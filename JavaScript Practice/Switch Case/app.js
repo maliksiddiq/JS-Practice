4ba2303a146401384ebbaddbff8ce406b801d5af
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-09-28 04:38:39 -0900

    Switch Case are Done

---
+++ JavaScript Practice/Switch Case/app.js
@@ -0,0 +1,180 @@
+// console.log("JavaScript Connected!")
+
+// let dayNumber = +prompt('Type a number 0 to 6 ')
+
+// if (dayNumber === 0) {
+//     console.log('It is Sunday today!')
+// }   
+// else if (dayNumber === 1) {
+//     console.log('It is Monday today!')
+// } 
+// else if (dayNumber === 2) {
+//     console.log('It is Tuesday today!')
+// }    
+// else if (dayNumber === 3) {
+//     console.log('It is Wednesday today!')
+// }  
+// else if (dayNumber === 4) { 
+//     console.log('It is Thursday today!')
+// }  
+// else if (dayNumber === 5) {
+//     console.log('It is Friday today!')
+// }  
+// else if (dayNumber === 6) {
+//     console.log('It is Saturday today!')
+// } 
+// else {
+//     console.log('Please enter a valid number!')
+// } 
+
+//  ------ Switch Case !
+
+// switch (dayNumber) {
+
+//     case 0 :
+//         console.log('It is Sunday today!')
+//     break   
+//     case 1 :
+//          console.log('It is Monday today!')
+//     break        
+//     case 2 :
+//         console.log('It is Tuesday today!')
+//     break       
+//     case 3 :
+//         console.log('It is Wednesday today!')
+//     break     
+//     case 4 :
+//          console.log('It is Thursday today!')
+//     break       
+//     case 5 : 
+//          console.log('It is Friday today!')
+//     break      
+//     case 6 : 
+//          console.log('It is Saturday today!') 
+//     break  
+//     default :
+//         console.log("Please enter a valid number!")                 
+// }  
+
+//   ------------------------------------------------------------------------------------------------------------------------------------ !
+
+// var userName = prompt('Type your Name!')
+// var userAge = prompt('Type your Age!')
+
+// if ( userAge >= 3 && userAge <= 8) {
+//     console.log(`${userName}: is a kid`)
+//     console.log(`${userName}: is playing football`)
+// }
+// else if ( userAge >= 9 && userAge <= 18) {
+//     console.log(`${userName}: is a School Student`)
+//     console.log(`${userName}: is Learning Maths / English`)
+// }
+// else if ( userAge >= 19 && userAge <= 24) {
+//     console.log(`${userName}: is a College Student`)
+//     console.log(`${userName}: is Learning Computer Science`)
+// }
+// else if ( userAge >= 25 && userAge <= 50) {
+//     console.log(`${userName}: is a Working Professional`)
+//     console.log(`${userName}: is a Full Stack Devloper`)
+// }
+// else if (userAge >= 51) {
+//     console.log(`${userName}: is Retired!`)
+// }
+// else {
+//     console.log('Please enter a valid age!')
+// }  
+
+//  -------------- Switch Case ! ------------------ 
+
+// var userName = "Malik Siddiq"
+// var userAge = 22
+
+// debugger
+
+// switch (true) {
+
+//     case ( userAge >= 3 && userAge <= 8 ) :
+//         console.log(`${userName}: is a kid`)
+//     break    
+//     case ( userAge >= 9 && userAge <= 18 ) :
+//         console.log(`${userName}: is a School Student`)
+//     break 
+//     case ( userAge >= 19 && userAge <= 24 ) :
+//          console.log(`${userName}: is a College Student`)
+//     break
+//     case ( userAge >= 25 && userAge <= 50 ) :
+//         console.log(`${userName}: is a Working Professional`)
+//     break    
+//     case (userAge >= 51) :
+//           console.log(`${userName}: is Retired!`)
+//     break
+//     default: 
+//     console.log("Please enter a valid number!")      
+// }
+
+// let grade = 'C'
+
+// switch (grade.toLowerCase()) {
+
+//     case 'a' : 
+//         console.log('Your score is between 85% to 100%')
+//         break
+//     case 'b' : 
+//         console.log('Your score is between 70% to 85%')
+//         break 
+//      case 'c' : 
+//         console.log('Your score is between 60% to 70%')
+//         break 
+//     case 'd' : 
+//         console.log('Your score is between 40% to 60%')
+//         break
+//     default: 
+//         console.log("Sorry you are failed!")      
+// }
+
+// var color = 'Green'
+
+// switch (color.toLowerCase()) {
+
+//     case 'red' :
+//         console.log("Stop!")
+//         break
+//     case 'yellow' :
+//         console.log('Wait!')
+//         break
+//     case 'green' :
+//         console.log('Go!')
+//         break
+//     default :
+//            console.log('Unknown Color')             
+// }
+
+// const x = 1;
+
+// switch (x) {
+
+//     case '1' :
+//         console.log('1')
+//         break
+//     case 1: 
+//         console.log(`${x} is Matched`)
+//         break        
+// }
+
+// const day = 3;
+
+// switch (day) {
+
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log('Weekday')
+//     break
+//     case 0:    
+//     case 6:
+//         console.log('Weekend')    
+// }
+
+// console.log('Program Ended!')
