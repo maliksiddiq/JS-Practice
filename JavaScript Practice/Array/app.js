b94f1240dc7739c9490e10d1b1f594aec6429ded
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-09-29 01:33:43 -0900

    Arrays Start

---
+++ JavaScript Practice/Array/app.js
@@ -0,0 +1,20 @@
+console.log('JavaScript Connected!');
+
+//  Simple Array 
+// let fruits = ['Apple' , 'Banana' , 'Orange' , 'Mango'];
+// console.log(fruits)
+
+// console.log(fruits[1]);
+
+// 2D Array  Multidimensional Array !
+
+// let number = [
+//     [1 , 2 , 3],
+//     [4 , 5 , 6],
+//     [7 , 8 , 9]
+// ]
+
+// console.log(number[0][1])
+// console.log(number[1][1])
+// console.log(number[2][2])
+
