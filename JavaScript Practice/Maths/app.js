2ca7c9af3dc7d42cb0d903a080157d323355be32
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-10-01 01:37:55 -0900

    Maths in js

---
+++ JavaScript Practice/Maths/app.js
@@ -0,0 +1,11 @@
+// console.log('JavaScript Connected!');
+
+// var a = 12;
+
+// console.log(a);
+// console.log(a + 12);
+// console.log(a - 2);
+// console.log(a / 2);
+// console.log(a % 2);
+// console.log(a * 2);
+// console.log(a ** 2);
