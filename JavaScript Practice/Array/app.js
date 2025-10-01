2f193a81e67bee07ef43fd7dd38848cd1deb5cf3
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-10-01 01:08:53 -0900

    Arrays are Done

--- JavaScript Practice/Array/app.js
+++ JavaScript Practice/Array/app.js
@@ -1,4 +1,4 @@
-console.log('JavaScript Connected!');
+// console.log('JavaScript Connected!');
 
 //  Simple Array 
 // let fruits = ['Apple' , 'Banana' , 'Orange' , 'Mango'];
@@ -193,5 +193,96 @@ console.log('JavaScript Connected!');
 // console.log(multiArray[0][2],multiArray[1][2]);
 
 // let join = multiArray[0].concat(multiArray[1]);
-// console.log(join)
+// console.log(join);
+
+
+// -------------------------------------------------------------------------------------------------------------------------------------
+
+
+// let fruits = ['Apple' ,  'Banana' , 'Orange'];
+// console.log(fruits);
+// console.log(fruits.length);
+// console.log(fruits[1]);
+
+// let studentName = ['Ali' , 'Usman' , 'Imran'];
+// let studentMarks = [50 , 60 , 70];
+// console.log(studentName);
+// console.log(studentMarks);
+
+
+// let classResult = [
+//     ["Ali" , 50],
+//     ['Usman' , 60],
+//     ['Imran' , 70 ]
+// ]
+
+// console.log(classResult);
+// console.log(classResult[1][1]);
+
+// let threeDArray = [
+
+//     [
+//         ['Ali' , 8],
+//         ['Usman' , 9],
+//         ['Rizwan' , 10]
+
+//     ],
+
+//     [
+//         ['Amir' , 11],
+//         ['Umer' , 12],
+//         ['Ramzan' , 13]
+
+//     ]
+
+// ]
+
+// console.log(threeDArray);
+// console.log(threeDArray[0][1]);
+// console.log(threeDArray[1][1]);
+
+//   Methoods 
+
+// let fruits = ['Apple' , 'Mango' , 'Banana'];
+
+// console.log(fruits);
+// console.log(fruits.push('Peach'));
+// console.log(fruits);
+// console.log(fruits.pop());
+// console.log(fruits);
+// console.log(fruits.shift());
+
+// console.log(fruits);
+// console.log(fruits.unshift('Peach'));
+// console.log(fruits);
+
+// console.log(fruits.indexOf('Banana'));
+// console.log(fruits.indexOf('Apple'));
+// console.log(fruits.indexOf('apple'));
+
+// console.log(fruits.includes('Mango'));
+// console.log(fruits.includes('cat'));
+
+// let num = [10 , 9 , 8 , 7 ,  6  ,  5  , 4 , 3 , 2 , 1];
+// console.log(num);
+// console.log(num.reverse());
+
+// let names = ['Dua' , 'Umer' , 'Ali'  , 'Amir' , 'Rizwan' , 'Ramzan' , 'Ayesha'];
+// console.log(names);
+// console.log(names.sort());
+
+// let number = [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 100 , 200 , 300];
+// console.log(number);
+// console.log(number.sort());
+
+// let fruits = ['Apple'  , 'Mango' , 'Banana' , 'Peach'];
+// console.log(fruits);
+// console.log(fruits.slice(1));
+// console.log(fruits.slice(1 , 3));
+// console.log(fruits);
+
+// let fruits = ['Apple'  , 'Mango' , 'Banana' , 'Peach'];
+// console.log(fruits);
+// console.log(fruits.splice(1,1,'Watermelon', 'Strawberry'));
+// console.log(fruits);
 
