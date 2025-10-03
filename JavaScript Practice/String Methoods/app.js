9a5ff160c8238b6ffc617fbb8530c63fe5f11526
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-10-02 19:44:09 -0900

    String Methoods

---
+++ JavaScript Practice/String Methoods/app.js
@@ -0,0 +1,94 @@
+console.log('JavaScript Connected!');
+
+//  String Methoods Practice !
+
+// String Methoods Without Urgument
+
+// .toUpperCase()
+
+// var studentName = 'malik Siddiq awan!';
+// var toUpperCase = studentName.toUpperCase();
+
+// console.log(studentName);
+// console.log(toUpperCase);
+
+
+// .toLowerCase()
+
+// var studentName = ' MALIK SIDDIQ AWAN!';
+// var toLowerCase = studentName.toLowerCase();
+// console.log(studentName);
+// console.log(toLowerCase);
+// console.log(studentName);
+
+
+// .trim()
+
+// var studentName = "         Malik Siddiq Awan!                                 ";
+// var removeSpaces = studentName.trim();
+// console.log(studentName);
+// console.log(removeSpaces);
+// console.log(studentName);
+
+
+// var studentName = "         Malik Siddiq Awan!                                 ";
+// var removeSpacesStart = studentName.trimStart();
+
+// console.log(studentName);
+// console.log(removeSpacesStart);
+
+// var studentName = "         Malik Siddiq Awan!                                 ";
+// var removeSpacesEnd = studentName.trimEnd();
+
+// console.log(studentName);
+// console.log(removeSpacesEnd)
+
+
+// String Methoods With Urgument
+
+// indexOf()
+
+// var studentName = 'Malik Siddiq Awan!'
+// console.log(studentName);
+// console.log(studentName.indexOf('k'));
+// console.log(studentName.indexOf('n'));
+
+
+// var  studentName = 'Malik Siddiq Awan!';
+// console.log(studentName);
+// console.log(studentName[4]);
+// console.log(studentName[16]);
+
+// .includes()
+
+// var  studentName = 'Malik Siddiq Awan!';
+// var chekLetter = studentName.includes('M');
+
+// console.log(studentName);
+// console.log(chekLetter);
+
+// var  studentName = 'Hello I am Malik Siddiq Awan!';
+// var replace = studentName.replace('Hello' , 'Hi');
+// console.log(studentName);
+// console.log(replace);
+
+// var  studentName = 'Hello I am Malik Siddiq Awan!';
+// var replace = studentName.replace(' Siddiq' , ' Aqib ');
+// console.log(studentName);
+// console.log(replace);
+
+// var word = 'Congrats';
+// var changeWord = word.replace('Congrats' , 'Congrats!');
+// console.log(word); 
+// console.log(changeWord);
+
+// var sameWord = 'Malik Siddiq awan';
+// var changeWords = sameWord.replace('a' , 'u');
+// console.log(sameWord);
+// console.log(changeWords)
+
+
+// var sameWord = 'Malik Siddiq awan';
+// var changeWords = sameWord.replaceAll('a' , 'u');
+// console.log(sameWord);
+// console.log(changeWords);
