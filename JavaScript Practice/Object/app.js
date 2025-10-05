20893e51d78c0edad11ec0c0381ba470f47cc9e2
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-10-05 05:39:59 -0900

    Ojects Start!

---
+++ JavaScript Practice/Object/app.js
@@ -0,0 +1,203 @@
+// console.log('JavaScript Connected!');
+
+// let firstName = "";
+// let lastName = '';
+
+// console.log(firstName === lastName);
+
+// let userName1 = [];
+// let userName2 = [];
+
+// console.log(userName1)
+// console.log(userName2)
+
+// console.log(userName1 === userName2);
+
+// let person = { name:  'Ali' , role:  'Student'};
+// person.age = 20;
+
+// console.log(person);
+
+// let naya = new Object ();
+// console.log(naya);
+// naya.first_name = 'Malik' , naya.last_name = 'Siddiq' ;
+// console.log(naya);
+// naya.last_name = 'Aqib';
+// console.log(naya);
+// naya.age = 19;
+// console.log(naya);
+// naya.age = 22;
+// console.log(naya);
+// delete naya.age;
+// console.log(naya);
+// naya.age = 27;
+// console.log(naya);
+// naya.age = 32;
+// console.log(naya);
+// naya.tongue = 'Urdu';
+// console.log(naya);
+
+
+// let studentDetails = {
+//     first_name : "Malik",
+//     last_name :  "Siddiq",
+//     age : 20,
+//     isGraduate : false,
+//     city : 'Karachi'      
+// }
+
+// console.log(studentDetails);
+
+
+// let studentDetails = new Object();
+// studentDetails.first_name = 'Malik'
+// studentDetails.last_name = "Siddiq"
+// studentDetails.age = 20
+// studentDetails.isGraduate = false
+// studentDetails.city = 'Karachi'
+
+// console.log(studentDetails);
+
+
+// let studentDetails = new Object ();
+
+// studentDetails.first_name = 'Malik' , studentDetails.last_name = 'Siddiq'
+
+// console.log(studentDetails);
+
+
+// let arr = [1 , 2 , 3 , 4 , 5 ];
+
+// console.log(arr);
+// console.log(arr[4]);
+// console.log(typeof arr);
+
+
+// let obj = {
+//     num1 : 1,
+//     num2 : 2,
+//     num3 : 3,
+//     num4 : 4,
+//     num5 : 5
+// }
+
+// console.log(obj);
+// console.log(obj.num5);
+// console.log(typeof obj);
+
+
+// let obj  = {
+//     name : 'Aqib',
+//     father_name : 'Sabir',
+//     age : 22,
+//     qualification : 'Matric',
+//     otherDetails : {
+//         city_name : 'Karachi',
+//         mother_tongue : 'English',
+//         religion : 'Islam'
+//     }
+// }
+
+// console.log(obj);
+// console.log(obj.otherDetails);
+// console.log(obj.otherDetails.city_name);
+// console.log(typeof obj.otherDetails.city_name);
+
+
+// let studentDetails = {
+//     first_name : 'Saad',
+//     last_name : 'Ali',
+//     father_name : 'Adeel',
+//     age : 24,
+//     otherDeatils : {
+//         city_name : 'Karachi',
+//         qualification : 'Matric',
+//         tongue : 'English',
+//     }
+// } 
+
+// console.log(studentDetails);
+// console.log(studentDetails.age);
+// console.log(typeof studentDetails.age);
+// console.log(typeof studentDetails.last_name);
+// console.log(studentDetails.otherDeatils);
+// console.log(studentDetails.otherDeatils.city_name);
+// console.log(studentDetails.otherDeatils.qualification);
+// console.log(studentDetails.otherDeatils.tongue);
+
+
+// let islamic_Countries = {
+//     country1 : 'Pakistan',
+//     country2 : 'Afghanistan',
+//     country3 : 'Saudia Arabia',
+//     country4 : 'Bangladesh',
+//     country5 : 'Seria',
+// }
+
+// console.log(islamic_Countries);
+
+// let countriesName = {
+//     english_countries : {
+//        e_coun1 : 'Australia', 
+//        e_coun2 : 'England', 
+//        e_coun3 : 'Scotland', 
+//        e_coun4 : 'Switzerland', 
+//        e_coun5 : 'Japan',
+//        islamic_Countries 
+//     }
+// }
+
+// console.log(countriesName);
+
+
+// let countriesName = {
+//     english_countries : {
+//         e_coun1 : "Australia",
+//         e_coun2 : "England",
+//         e_coun3 : "Scotland",
+//         e_coun4 : "Switzerland",
+//         e_coun5 : "Japan",
+//         islamic_countries : {
+//             i_coun1 : "Pakistan",
+//             i_coun2 : "Afghanistan",
+//             i_coun3 : "Saudia Arabia",
+//             i_coun4 : "Bangldesh",
+//             i_coun5 : "Seria",
+//         }
+//     }
+// }
+
+// console.log(countriesName);
+// console.log(countriesName.english_countries);
+// console.log(countriesName.english_countries.islamic_countries);
+
+// last
+
+// console.log(countriesName.english_countries.e_coun5);
+// console.log(countriesName.english_countries.islamic_countries.i_coun5);
+
+
+// let subjectsName = {
+//     English_Sub : {
+//         e_sub1 : "English",
+//         e_sub2 : "Biology",
+//         e_sub3 : "Physic",
+//         e_sub4 : "Chemistry",
+//         Urdu_Sub : {
+//             u_sub1 : "Urdu",
+//             u_sub2 : "Islamiat",
+//             u_sub3 : "Serat",
+//         }
+//     }
+// }
+
+// subjectsName.English_Sub.e_sub3 = "Maths";      Replace
+
+// subjectsName.English_Sub.Urdu_Sub.u_sub4 = "Mutala Pakistan";      Add
+// subjectsName.English_Sub.Urdu_Sub.u_sub5 = "General Knowledge";  Add
+
+// delete subjectsName.English_Sub.e_sub2;  Delete
+
+// console.log(subjectsName);
+// console.log(subjectsName.English_Sub);
+// console.log(subjectsName.English_Sub.Urdu_Sub);
