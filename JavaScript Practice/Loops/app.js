e89c1a336927980c277574ffe1a402fcc28b04f0
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-10-19 12:03:07 -0900

    For loop are done!

---
+++ JavaScript Practice/Loops/app.js
@@ -0,0 +1,170 @@
+// console.log('Js Connected!');
+
+//  Loops Syntax!
+
+// for loop
+
+// for(let i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
+
+
+// While loop
+
+// let i = 0;
+
+// while(i <= 10) {
+//     console.log(i)
+//     i++
+// }
+
+
+//  Do While loop
+
+// let i = 0;
+
+// do {
+//     console.log(i)
+//     i++ 
+// }while(i <= 10)
+
+// ------------------------------------------------------------------------------------------
+
+// for(let i = 0; i <= 100; i++) {
+
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+
+// let arr = ['Siddiq' , 'Asif' , 'Ali' , 'Aqib' , 'Abdullah' , 'Qasim' , 'Mahboob' , 'Faizan' , 'Hamza']
+
+// for(let i = 0; i < arr.length; i++) {
+//    console.log(arr[i]) 
+// }
+
+
+// let sum = 0;
+
+// for(let i = 1; i <= 10; i++){
+//    sum = sum + i 
+// }
+
+// console.log(sum);
+
+
+// let sum = 0;
+
+// for (let i = 1; i <= 20; i++){
+
+//     if(i % 2 === 0){
+//        sum = sum + i 
+//     }
+// }
+
+// console.log(sum)
+
+
+// var total = 0;
+
+// let arr = [10, 20, 30, 40, 50]
+
+// for(let i = 0; i < arr.length; i++) {
+//   total = total + arr[i]
+// }
+
+// console.log(total)
+
+
+// for(let i = 0; i <= 10; i++){
+
+//     if(i % 2  !== 0) {
+//         console.log(i)
+//     }
+// }
+
+
+// for(let i = 0; i <= 10; i++){
+
+//     if(! ( i % 2  === 0 )) {
+//         console.log(i)
+//     }
+// }
+
+
+// for(let i = 0; i <= 10; i++){
+
+//     if(! ( i % 2  !== 0 )) {
+//         console.log(i)
+//     }
+// }
+
+
+// --------------------------------------------------------------------------------------------
+
+// let i = 0;
+
+// while(i <= 100) {
+
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }i++
+// }
+
+
+// let arr = ['Siddiq' , 'Asif' , 'Ali' , 'Aqib' , 'Abdullah' , 'Qasim' , 'Mahboob' , 'Faizan' , 'Hamza' , 'Rehan' , 'Shazil   ']
+
+// let i = 0;
+
+// while(i < arr.length) {
+//     console.log(arr[i])
+//     i++
+// }
+
+
+// let i = 0;
+
+// while(i < 5) {
+//     console.log('Hello JavaScript!')
+//     i++
+// }
+
+// --------------------------------------------------------------------------------------------
+
+// let i = 0;
+
+// do {
+//     if(i % 2 === 0) {
+//         console.log(i)
+//     }
+//     i++
+// }while(i <= 100)
+
+
+// let arr = ['Siddiq' , 'Asif' , 'Ali' , 'Aqib' , 'Abdullah' , 'Qasim' , 'Mahboob' , 'Faizan' , 'Hamza' , 'Rehan' , 'Shazil']
+
+// let i = 0;
+
+// do {
+//     console.log(arr[i])
+//     i++
+// }while(i < arr.length)
+
+// let i = 0;
+
+// do {
+//     console.log(i)
+//     i++
+// }while(i < 4)
+
+
+// let i = 0;
+
+// do {
+
+//     if(i % 2 === 0)  {
+//         console.log(i)
+//     }
+//     i++
+// }while(i <= 100)
