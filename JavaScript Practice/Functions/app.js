50a23e21739d9411a8c86f172b38cb487f2fdf21
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-10-19 20:51:17 -0900

    Functions are Start

---
+++ JavaScript Practice/Functions/app.js
@@ -0,0 +1,43 @@
+// console.log('JS Connected!')
+
+// Functions
+
+// JavaScript me function ek block of code (code ka group) hota hai jo bar-bar use kiya ja sakta hai.
+// Yaani ek baar likho, aur jab chaho use (call) kar lo.
+
+// function introduceMe(userName) {
+//     console.log(`My name is ${userName}`)
+//     console.log('I am a Web Developer')
+//     console.log('i am 19 years old')
+// } 
+
+// introduceMe('Aqib')
+// introduceMe('Qasim')
+// introduceMe('Mazhar')
+
+
+// function introduceMe(userName , profession , age) {
+//     console.log(`My name is ${userName}`)
+//     console.log(`I am  a ${profession}`)
+//     console.log(`I am ${age} Years Old `)
+//     console.log('***************************')
+// } 
+
+// introduceMe('Siddiq' , 'Web Developer' , 19)
+// introduceMe('Aqib' , 'Ui UX Designer' , 20)
+// introduceMe('Qasim' , 'Graphic Designer' , 22)
+
+
+// function introduceSelf(userName = 'Procodr') {
+//     console.log(`My Name is ${userName}`)
+// }
+
+// introduceSelf()
+
+
+// function introduceSelf(userName ) {
+//     console.log(`My Name is ${userName || 'Procodr'}`)
+// }
+
+// introduceSelf('Malik')
+
