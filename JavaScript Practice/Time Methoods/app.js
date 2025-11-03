a5633ba0bf3000938608e3144ea9d694a89e5dd7
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-11-03 04:11:41 -0900

    Time Methoods

---
+++ JavaScript Practice/Time Methoods/app.js
@@ -0,0 +1,41 @@
+console.log('Js Connected!');
+
+//   setTimeout (function, time)
+//  Ye ek function ko delay ke baad sirf ek baar chalata hai.
+
+// setInterval(function, time)
+// Ye bar-bar same time gap ke baad function ko repeat karta hai.
+
+
+// clearTimeout()    //    clearInterval()
+// Ye dono stop / cancel karte hain un timers ko jo upar diye gaye hain.
+
+
+// setTimeout(first , 1000)
+// setTimeout(sec , 2000)
+// setTimeout(third , 3000)
+
+// function first() {
+//    console.log('First Function!') 
+// }
+
+// function sec() {
+//    console.log('Sec Function!') 
+// }
+
+// function third() {
+//     console.log('Third Function!')
+// }
+
+
+// function time() {
+//     console.log('Start Function!')
+// }
+
+// const save = setTimeout(time , 2000)
+// console.log(save)
+// console.log(clearTimeout(save))
+
+
+
+
